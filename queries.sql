5c984e0ba60fe6f81eef73f59913acc11d86024c
Carl <carljarms@gmail.com>
2020-03-14 03:18:08 -0500

    completed models, repositories, and controllers

--- queries.sql
+++ queries.sql
@@ -1,6 +1,15 @@
 ## Part 1: Test it with SQL
 
+    select column_name, data_type
+    from information_schema.columns
+    where table_schema = 'techjobs'
+    and table_name = 'job';
+
 ## Part 2: Test it with SQL
+
+    select name
+    from employer
+    where location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
